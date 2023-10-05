39b0d50e79798d0705a7703ba98fc09db3752a2e
Brandon Chee <brandonch9029@gmail.com>
2023-10-05 10:25:02 +0100

    Update task1.cpp

--- Practical1/task1.cpp
+++ Practical1/task1.cpp
@@ -44,10 +44,10 @@ int main()
       else{
           int num_teams= omp_get_num_teams();
           int num_threads_per_team = omp_get_num_threads();
-        printf("Running on GPU with %d teams and %d threads per team\n", 
+      }
+  }
+          printf("Running on GPU with %d teams and %d threads per team\n", 
           num_teams, 
           num_threads_per_team
         );
-      }
-  }
 }
