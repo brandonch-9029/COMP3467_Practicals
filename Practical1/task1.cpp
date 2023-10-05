7a1bcce7020d74b3d937d32f3166c1168b1c76c8
Brandon Chee <brandonch9029@gmail.com>
2023-10-05 10:21:42 +0100

    Update task1.cpp

--- Practical1/task1.cpp
+++ Practical1/task1.cpp
@@ -35,7 +35,7 @@ int main()
   int num_devices = omp_get_num_devices();
   printf("Number of available target devices %d\n", num_devices);
 
-  #pragma omp target teams distribute parallel for
+  #pragma omp target teams distribute parallel
   {
       if(omp_is_initial_device())
       {
