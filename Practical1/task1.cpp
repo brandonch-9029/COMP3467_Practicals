bc800c1caf3e74fe9c0cf2ff9bb9ad5e3193ccdc
Brandon Chee <brandonch9029@gmail.com>
2023-10-05 10:21:15 +0100

    Update task1.cpp

--- Practical1/task1.cpp
+++ Practical1/task1.cpp
@@ -35,22 +35,19 @@ int main()
   int num_devices = omp_get_num_devices();
   printf("Number of available target devices %d\n", num_devices);
 
-  #pragma omp target teams
+  #pragma omp target teams distribute parallel for
   {
       if(omp_is_initial_device())
       {
         printf("Running on CPU\n");    
       }
       else{
-        #pragma omp parallel
-        {
           int num_teams= omp_get_num_teams();
           int num_threads_per_team = omp_get_num_threads();
         printf("Running on GPU with %d teams and %d threads per team\n", 
           num_teams, 
           num_threads_per_team
         );
-        }
       }
   }
 }
