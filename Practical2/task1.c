91720aca49a66f12555cfd088dd886168d79503d
Brandon Chee <brandonch9029@gmail.com>
2023-10-12 10:52:47 +0100

    Update task1.c

--- Practical2/task1.c
+++ Practical2/task1.c
@@ -33,7 +33,7 @@ int main(int argc, char** argv) {
         MPI_Send(&message, 14, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
         printf("Rank 0 has sent a message to Rank 1\n");
     } else if (rank == 1) {
-        int msg_size = 0;
+        int msg_size;
         MPI_Status status;
         MPI_Probe(0, 0, MPI_COMM_WORLD, &status);
         MPI_Get_count(&status, MPI_INT, &msg_size);
