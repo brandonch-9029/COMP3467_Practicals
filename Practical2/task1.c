a9e8a1fb82379bd956f40812a28863f3cb337534
Brandon Chee <brandonch9029@gmail.com>
2023-10-12 10:35:56 +0100

    Update task1.c

--- Practical2/task1.c
+++ Practical2/task1.c
@@ -31,12 +31,12 @@ int main(int argc, char** argv) {
     if (rank == 0) {
         char message[] = "Hello World!\n";
         MPI_Send(&message, 14, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-        printf("Rank 0 has sent a message to Rank 1")
+        printf("Rank 0 has sent a message to Rank 1");
     } else if (rank == 1) {
         char message[64];
         //
         MPI_Recv(&message, 14, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Rank 1 has received a message from Rank 0:\n%s",&message);
+        printf("Rank 1 has received a message from Rank 0:\n%s",message);
     }
     
     MPI_Finalize();
